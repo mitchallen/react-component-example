ca34cab9ac5cea414de2ff04f3af7fa8b722a1e1
Mitch Allen <mitch.c.allen@gmail.com>
2019-10-04 06:04:55 -0400

    added gitignore and removed dist

--- dist/src/ComponentExample.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-interface Props {
-}
-interface IState {
-}
-export declare class ComponentExample extends React.Component<Props, IState> {
-    render(): JSX.Element;
-}
-export {};
